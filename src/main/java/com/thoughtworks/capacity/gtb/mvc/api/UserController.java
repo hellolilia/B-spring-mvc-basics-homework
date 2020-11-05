3c53051f43fdbed5bf5167e16255ccb696a01640
hellolilia <70508924+hellolilia@users.noreply.github.com>
2020-11-05 21:48:17 +0800

    fix: fix import

--- src/main/java/com/thoughtworks/capacity/gtb/mvc/api/UserController.java
+++ src/main/java/com/thoughtworks/capacity/gtb/mvc/api/UserController.java
@@ -4,7 +4,12 @@ import com.thoughtworks.capacity.gtb.mvc.domain.User;
 import com.thoughtworks.capacity.gtb.mvc.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
--- src/main/java/com/thoughtworks/capacity/gtb/mvc/domain/User.java
+++ src/main/java/com/thoughtworks/capacity/gtb/mvc/domain/User.java
@@ -4,7 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 
 @Data
 @NoArgsConstructor
